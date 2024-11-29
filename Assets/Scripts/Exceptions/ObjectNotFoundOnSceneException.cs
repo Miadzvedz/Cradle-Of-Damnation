be0ffd61437fce04491c6b3810d62a686eabfb85
Alexander Medved <medved1991alexander@gmail.com>
2024-11-29 23:01:29 +0300

    PlayerManager

---
+++ Assets/Scripts/Exceptions/ObjectNotFoundOnSceneException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public class ObjectNotFoundOnSceneException : Exception
+    {
+        public ObjectNotFoundOnSceneException(string message) : base(message) 
+        {
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Exceptions;
+using UnityEngine;
+
+namespace Managers
+{
+    public sealed class PlayerManager : BaseManager<PlayerManager>
+    {
+        [field: SerializeField] 
+        public Player Player { get; private set; }
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            Player ??= FindObjectOfType<Player>() ?? throw new ObjectNotFoundOnSceneException(Player.name);
+        }
+    }
+}
